ba537c6a1cca00dc49bd97689c4167f00872c5f2
Vishaljain290502 <vishalj.aurasoft@gmail.com>
2024-10-18 17:59:28 +0530

    push

--- src/course/course.schema.ts
+++ src/course/course.schema.ts
@@ -3,6 +3,40 @@ import { Document } from 'mongoose';
 
 export type CourseDocument = Course & Document;
 
+export enum ContentType {
+  Video = 'video',
+  Article = 'article',
+  Quiz = 'quiz',
+}
+
+@Schema()
+export class Content {
+  @Prop({ enum: ContentType, required: true })
+  type: ContentType;
+
+  @Prop({ required: true })
+  title: string;
+
+  @Prop({ required: true })
+  url: string; 
+
+  @Prop()
+  quizId?: string;  
+}
+
+export const ContentSchema = SchemaFactory.createForClass(Content);
+
+@Schema()
+export class Lesson {
+  @Prop({ required: true })
+  title: string;
+
+  @Prop({ type: [ContentSchema], required: true })
+  contents: Content[];
+}
+
+export const LessonSchema = SchemaFactory.createForClass(Lesson);
+
 @Schema()
 export class Course {
   @Prop({ required: true })
@@ -11,8 +45,8 @@ export class Course {
   @Prop({ required: true })
   description: string;
 
-  @Prop({ required: true })
-  lessons: string[];
+  @Prop({ type: [LessonSchema], required: true })
+  lessons: Lesson[];
 
   @Prop({ required: true })
   instructor: string;
